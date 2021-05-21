fcbcc01c71fa32f7a51a41a5f586f415a0742a31
yong <2drakestone@gmail.com>
2021-05-21 09:53:34 +0900

    설명 추가

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import serial
 
+# git add (푸시하고싶은 파일) 또는 git add . (전체파일)
+# git commit -m "하고싶은 말"
+# git push
+
 arduino = serial.Serial('COM3', 9600)
 
 
